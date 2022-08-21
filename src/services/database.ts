16345a3241238f5e5c78ae480680c8b1127aaad7
Saulo-Felipe <saulofelipe234567@gmail.com>
2022-08-21 15:06:50 +0000

    fix pg error

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,5 +1,7 @@
 // postgres://diiexwhigbnhjq:9ec7c7771cf4ab1fc586a36d62b2d0abcbe0e18fe3d1bea2cae9bd0673b50131@ec2-52-205-61-230.compute-1.amazonaws.com:5432/d3t3cqqdugu9d9/postgres?ssl=true&sslmode=require
+import * as pg from 'pg';
 import { Sequelize } from "sequelize";
+
 
 export const sequelize = new Sequelize('d3t3cqqdugu9d9', 'diiexwhigbnhjq', '9ec7c7771cf4ab1fc586a36d62b2d0abcbe0e18fe3d1bea2cae9bd0673b50131', {
   host: 'ec2-52-205-61-230.compute-1.amazonaws.com',
@@ -10,4 +12,5 @@ export const sequelize = new Sequelize('d3t3cqqdugu9d9', 'diiexwhigbnhjq', '9ec7
       rejectUnauthorized: false 
     },
   },
+  dialectModule: pg
 });
