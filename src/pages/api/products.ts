f025511676b7cf83eeed1305fa0a91e86f765273
Saulo-Felipe <saulofelipe234567@gmail.com>
2022-08-21 14:29:09 +0000

    api

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -2,12 +2,18 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { sequelize } from "../../services/database";
 
 export default async function Products(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    if (request.method === "GET") {
+      const [products] = await sequelize.query(`
+        SELECT * FROM products
+      `);
+  
+      return response.json({ products });
+    }
 
-  if (request.method === "GET") {
-    const [products] = await sequelize.query(`
-      SELECT * FROM products
-    `);
-
-    return response.json({ products });
+    throw true;
+  } catch(e) {
+    console.log(e);
+    return response.json({ message: e });
   }
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,20 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_HOSTNAME+"/api",
 });
+
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    alert("Erro");
+    console.log("Response: ", error);
+  }
+);
+
+api.interceptors.request.use(
+  request => request,
+  error => {
+    alert("Erro");
+    console.log("Request: ", error);
+  }
+)
